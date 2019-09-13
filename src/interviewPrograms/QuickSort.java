82d7ad236f4d1eeb199709215a5a5b09dcf2a1f3
bishwajit01 <bishwajit.vikram@gmail.com>
2019-09-13 16:29:06 +0530

    adding quicksort program

---
+++ src/interviewPrograms/QuickSort.java
@@ -0,0 +1,55 @@
+package interviewPrograms;
+
+/**
+ * @author Bishwajit.
+ *
+ */
+public class QuickSort {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int []arr = {10, 2, 30, 4, 50};
+		System.out.print("Unsorted Array::\t");
+		display(arr);
+		sort(arr, 0, arr.length-1);
+		System.out.print("Sorted  Array ::\t");
+		display(arr);
+	}
+
+	static int partition(int arr[], int low, int high) {
+		int pivot = arr[high];
+		int i = low - 1;
+		for (int j = low; j < high; j++) {
+			if (arr[j] < pivot) {
+				i++;
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		int temp = arr[i + 1];
+		arr[i + 1] = arr[high];
+		arr[high] = temp;
+
+		return i + 1;
+	}
+
+	static void sort(int ar[], int low, int high) {
+		if (low < high) {
+			int p = partition(ar, low, high);
+			sort(ar, low, p - 1);
+			sort(ar, p + 1, high);
+		}
+	}
+
+	static void display(int ar[]) {
+		for (int i = 0; i < ar.length; i++) {
+			System.out.print(ar[i] + " ");
+		}
+		System.out.println();
+	}
+
+}
