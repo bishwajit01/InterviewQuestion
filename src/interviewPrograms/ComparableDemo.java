9fc5ae3b556302361dea361cdeacb6e727bb4124
bishwajit01 <bishwajit.vikram@gmail.com>
2019-09-16 11:41:22 +0530

    Comparable in Collections.

--- src/interviewPrograms/ComparableDemo.java
+++ src/interviewPrograms/ComparableDemo.java
@@ -3,9 +3,15 @@
  */
 package interviewPrograms;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * @author bvikram2
- *
+ * @author Bishwajit.
+ * Comparable is Single Sorting.
+ * It uses compareTo method.
+ * It affect the original class. It modifies the data of the original class.
  */
 public class ComparableDemo {
 
@@ -13,8 +19,79 @@ public class ComparableDemo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Student> listStudent = new ArrayList<Student>();
 
+		listStudent.add(new Student("A", 49, 90000L));
+		listStudent.add(new Student("B", 34, 24000L));
+		listStudent.add(new Student("C", 30, 56000L));
+		listStudent.add(new Student("D", 35, 69000L));
+
+		Collections.sort(listStudent);
+
+		for (Student student : listStudent) {
+			System.out.println(student.getName() + " " + student.getAge() + " " + student.getSal());
+		}
+	}
+
+}
+
+final class Student implements Comparable<Student> {
+	private String name;
+	private int age;
+	private long sal;
+
+	public Student(String name, int age, long sal) {
+		this.name = name;
+		this.age = age;
+		this.sal = sal;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the sal
+	 */
+	public long getSal() {
+		return sal;
+	}
+
+	/**
+	 * @param dsl the sal to set
+	 */
+	public void setSal(long sal) {
+		this.sal = sal;
+	}
+
+	@Override
+	public int compareTo(Student student) {
+		// TODO Auto-generated method stub
+		return this.age - student.age;
 	}
 
 }
