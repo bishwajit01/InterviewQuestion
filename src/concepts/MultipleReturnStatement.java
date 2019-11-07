f7a171ba3697bfe5b5475cc27e95a89ee5bacaff
bishwajit01 <bishwajit.vikram@gmail.com>
2019-11-07 17:50:13 +0530

    Different Scenarios when using multiple different return statement.

--- src/concepts/MultipleReturnStatement.java
+++ src/concepts/MultipleReturnStatement.java
@@ -7,16 +7,51 @@ package concepts;
 public class MultipleReturnStatement {
 
 	public static void main(String[] args) {
-		// 3 will be returned, whether Exception is there or not.		
-		System.out.println("Returned value :: " + returningInt());
+
+		// 3 will be returned, whether Exception is there or not.
+		System.out.println("Returned value when Exception :: " + returnStatement1());
+		System.out.println("Returned value when no Exception :: " + returnStatement2());
+		
+		// it wont execute
+		System.out.println("Returned value when you dont want finally to execute :: " + returnStatement3());
 	}
 
-	public static int returningInt() {
-		try  {
-			return 1/0;
+	public static int returnStatement1() {
+		try {
+			System.out.println("Try");
+			return 1 / 0;
 		} catch (Exception e) {
+			System.out.println("Catch");
 			return 2;
 		} finally {
+			System.out.println("Finally");
+			return 3;
+		}
+	}
+
+	public static int returnStatement2() {
+		try {
+			System.out.println("Try");
+			return 1;
+		} catch (Exception e) {
+			System.out.println("Catch");
+			return 2;
+		} finally {
+			System.out.println("Finally");
+			return 3;
+		}
+	}
+
+	public static int returnStatement3() {
+		try {
+			System.out.println("Try");
+			System.exit(0);
+			return 1;
+		} catch (Exception e) {
+			System.out.println("Catch");
+			return 2;
+		} finally {
+			System.out.println("Finally");
 			return 3;
 		}
 
