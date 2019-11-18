7d35453f6714a5767e38630c6936f56744cb7f0f
bishwajit01 <bishwajit.vikram@gmail.com>
2019-11-19 00:34:45 +0530

    Updated the code, and removed the unnecessary code.

--- src/concepts/DeadlockInSingleThread.java
+++ src/concepts/DeadlockInSingleThread.java
@@ -6,14 +6,13 @@ package concepts;
  */
 public class DeadlockInSingleThread {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		try {
 			System.out.println("DeadLock Started :: ");
+			System.out.println("Thread Id :: " + Thread.currentThread().getId());
+			System.out.println("Thread Name :: " + Thread.currentThread().getName());
 			Thread.currentThread().join();
 			System.out.println("This Statement wont execute");
 		} catch (InterruptedException e) {
