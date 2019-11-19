2dd469268e327c57b971341adbdd243d74fc1083
bishwajit01 <bishwajit.vikram@gmail.com>
2019-11-19 22:07:45 +0530

    Two More Usage of Optional.

--- src/concepts/OptionalDemo.java
+++ src/concepts/OptionalDemo.java
@@ -19,6 +19,18 @@ public class OptionalDemo {
 		} else {
 			System.out.println("The value is null.");
 		}
+
+		str[2] = "Geeks Classes are coming soon";
+
+		// It returns an empty instance of Optional class
+		Optional<String> empty = Optional.empty();
+		System.out.println(empty);
+		// System.out.println(empty.get());// Exception:: No Such Element Present
+
+		// It returns a non-empty Optional
+		Optional<String> value = Optional.of(str[2]);
+		System.out.println(value);
+		System.out.println(value.get());
 	}
 
 }
