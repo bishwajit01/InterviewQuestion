05eb8213397ad03313d7149e9090243f6fe9c599
bishwajit01 <bishwajit.vikram@gmail.com>
2019-12-04 20:07:44 +0530

    Creating a Singleton using the final keyword.

---
+++ src/interviewPrograms/SingletonUsingFinalKeyword.java
@@ -0,0 +1,15 @@
+package interviewPrograms;
+
+/**
+ * @author Bishwajit.
+ * 
+ * This is using the final keyword.
+ */
+public class SingletonUsingFinalKeyword {
+
+	public static final SingletonUsingFinalKeyword instance = new SingletonUsingFinalKeyword();
+
+	private SingletonUsingFinalKeyword() {
+	}
+
+}
