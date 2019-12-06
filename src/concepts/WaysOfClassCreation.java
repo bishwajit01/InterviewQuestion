42be2e84f55fac0c1b04bc4054f8d1ace3d92647
bishwajit01 <bishwajit.vikram@gmail.com>
2019-12-06 18:42:35 +0530

    Ways of creation of class, what all modifiers we can add before the class.

---
+++ src/concepts/WaysOfClassCreation.java
@@ -0,0 +1,19 @@
+package concepts;
+
+/**
+ * @author Bishwajit.
+ * 
+ * Ways of Class Creation 
+ * 1. We can only have one Public Class.
+ * 2. Class with no Access Modifiers.
+ * 3. Protected and Private not valid, else Compile time Error saying "Remove invalid Modifiers".
+ *
+ */
+public class WaysOfClassCreation {
+
+}
+
+class ClassCreationNoAccessModifiers {
+}
+// protected class ClassCreationUsingProtected {}
+// private class ClassCreationUsingPrivate {}
