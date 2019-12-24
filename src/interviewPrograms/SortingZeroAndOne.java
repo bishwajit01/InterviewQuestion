4bb07d78d5a563d774a3d30bee5e613e09d8db1f
bishwajit01 <bishwajit.vikram@gmail.com>
2019-12-24 12:53:34 +0530

    Sorting using placing.

--- src/interviewPrograms/SortingZeroAndOne.java
+++ src/interviewPrograms/SortingZeroAndOne.java
@@ -11,9 +11,14 @@ public class SortingZeroAndOne {
 	public static void main(String[] args) {
 		int arr[] = { 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0 };
 		System.out.println("LENGTH of array:: " + arr.length);
+
+		// Using Counting Sorting.
 		sortingUsingCounting(arr);
+		// Sorting Using Placing
+		sortingUsingPlacing(arr);
 	}
 
+	// Sorting using Counting.
 	private static void sortingUsingCounting(int[] arr) {
 		int count = 0;
 		for (int i = 0; i < arr.length; i++) {
@@ -30,9 +35,30 @@ public class SortingZeroAndOne {
 				arr[i] = 1;
 			}
 		}
+		display(arr);
+	}
+
+	// Sorting Using Placing.
+	private static void sortingUsingPlacing(int[] arr) {
+		int length = arr.length;
+		int ar[] = new int[length];
+		int left = 0, right = length - 1;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				ar[left++] = 0;
+			} else {
+				ar[right--] = 1;
+			}
+		}
+		display(ar);
+	}
+
+	// Displaying the array.
+	static void display(int arr[]) {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
+		System.out.println();
 	}
 
 }
