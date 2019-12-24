74b5c2f6a769e91381e5d9da59448fed9e2ddec7
bishwajit01 <bishwajit.vikram@gmail.com>
2019-12-24 12:20:53 +0530

    Moved to a method.

--- src/interviewPrograms/SortingZeroAndOne.java
+++ src/interviewPrograms/SortingZeroAndOne.java
@@ -11,6 +11,10 @@ public class SortingZeroAndOne {
 	public static void main(String[] args) {
 		int arr[] = { 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0 };
 		System.out.println("LENGTH of array:: " + arr.length);
+		sortingUsingCounting(arr);
+	}
+
+	private static void sortingUsingCounting(int[] arr) {
 		int count = 0;
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] == 0) {
